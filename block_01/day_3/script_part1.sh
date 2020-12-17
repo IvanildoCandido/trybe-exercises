515bedbe859eb08b0f4bae842dda1d7b8681f7bb
Ivanildo Cândido <systechtecnologia@gmail.com>
2020-12-17 13:15:22 -0300

    Enviando exercicios bloco 1 dia 3

---
+++ block_01/day_3/script_part1.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# Exercise 1
+mkdir unix_tests
+cd ./unix_tests
+# Exercise 2
+touch trybe.txt
+# Exercise 3
+cp trybe.txt trybe_backup.txt
+# Exercise 4
+mv trybe.txt newTrybe.txt
+# Exercise 5
+mkdir backup
+# Exercise 6
+mv trybe_backup.txt backup
+# Exercise 7
+mkdir backup2
+# Exercise 8
+mv ./backup/trybe_backup.txt ./backup2
+# Exercise 9
+rmdir backup
+# Exercise 10
+mv backup2 backup
+# Exercise 11
+pwd
+# Exercise 12
+rm -rf backup2
+# Exercise 13
+clear
+# Exercise 14
+echo -e "Internet\nUnix\nBash\nHTML\nCSS\nJavaScript\nReact\nSQL" > skills.txt
+head -5 skills.txt
+# Exercise 15
+tail -4 skills.txt
+# Exercise 16
+
