1a8758dbf97a22cfce3b80c8df45c6265ce3bc38
Ivanildo Cândido <systechtecnologia@gmail.com>
2021-01-08 18:21:58 -0300

    Reorganizando as pastas do módulo 1

--- block_10/day_3/ex2.test.js
+++
@@ -1,9 +0,0 @@
-let randonNumber = require('./ex1.test');
-
-it('Testa a nova implementação', () => {
-  randonNumber = jest.fn().mockImplementationOnce((a, b) => a / b);
-  expect(randonNumber(4, 2)).toBe(2);
-  expect(randonNumber).toHaveBeenCalled();
-  expect(randonNumber).toHaveBeenCalledTimes(1);
-  expect(randonNumber).toHaveBeenCalledWith(4, 2);
-});
--- block_10/day_3/ext4.data.js
+++
@@ -1,5 +0,0 @@
-let strToUp = (str) => str.toUpperCase();
-let strFirstChar = (str) => str[0];
-let strConcat = (str1, str2) => str1 + str2;
-
-module.exports = { strToUp, strFirstChar, strConcat };
