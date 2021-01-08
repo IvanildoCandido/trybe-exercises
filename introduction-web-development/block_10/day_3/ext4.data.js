9ab6e4403b6bbb6a7e53d731578318bb4d6cc071
Ivanildo Cândido <systechtecnologia@gmail.com>
2021-01-08 18:16:21 -0300

    Reorganizando as pastas do módulo

---
+++ introduction-web-development/block_10/day_3/ext4.data.js
@@ -0,0 +1,5 @@
+let strToUp = (str) => str.toUpperCase();
+let strFirstChar = (str) => str[0];
+let strConcat = (str1, str2) => str1 + str2;
+
+module.exports = { strToUp, strFirstChar, strConcat };
